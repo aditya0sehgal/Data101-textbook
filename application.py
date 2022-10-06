8a9b6d02ee4b7d288197b691e05acb4c2f613d74
paramshah4 <paramjerry648@gmail.com>
2022-10-05 20:35:04 -0400

    handling long query execution

--- application.py
+++ application.py
@@ -31,30 +31,43 @@ def sqlTutorialCode():
     execution_time=0
     details=[]
     field_names=[]
-    conn = pymysql.connect(
-    host= "209.97.156.178",
-    port = 3307, 
-    user = "student", 
-    password = "cs336", 
-    db = "BarBeerDrinker", 
-    read_timeout=30
-    )
-    cur = conn.cursor(pymysql.cursors.DictCursor)
+    conn,cur = getSqlConnection()
     print("Start")
     try:
+        cur.execute("SELECT CONNECTION_ID() AS pid;")
+        pid=cur.fetchone()['pid']
+        print(pid)
         start_time = time.time()
         cur.execute(sqlcode)
         end_time=time.time()
         execution_time=end_time-start_time
         details=cur.fetchall()
         field_names = [i[0] for i in cur.description]
-        
     except pymysql.Error as e:
+        if(str(e)=="(2013, 'Lost connection to MySQL server during query (timed out)')"):
+            conn1,cur1 = getSqlConnection()
+            cur1.execute("KILL "+str(pid))
+            cur1.close()
+            conn1.close()
         print("Errorr")
         error = "Error occured: " + str(e)
     cur.close()
     conn.close()
     return jsonify({"result":details,"cols":field_names,"error":error,"execution":execution_time})
+
+
+def getSqlConnection():
+    conn = pymysql.connect(
+    host= "209.97.156.178",
+    port = 3307, 
+    user = "student", 
+    password = "cs336", 
+    db = "BarBeerDrinker", 
+    read_timeout=30,
+    autocommit=True
+    )
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    return conn,cur
 
 if __name__ == "__main__":
     application.run(port=5001,debug=True)
