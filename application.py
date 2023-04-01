8f90bac57b112896fbd2521256b9fab7501b4fc9
paramshah4 <paramjerry648@gmail.com>
2023-04-01 13:27:34 -0400

    analytics dashboard

--- application.py
+++ application.py
@@ -10,6 +10,28 @@ from pymongo import MongoClient
 import bson.json_util as json_util
 import json
 import pandas as pd
+import schedule
+import time
+
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+import flask_profiler
+import flask_monitoringdashboard as dashboard
+
+
+
+# def analytics_log():
+#     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=print_date_time, trigger="interval", seconds=60)
+# scheduler.start()
+
+# # Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
 
 class JsonEncoder(JSONEncoder):
     def default(self, obj):
@@ -23,20 +45,21 @@ class JsonEncoder(JSONEncoder):
 application = Flask(__name__)
 CORS(application)
 application.json_encoder = JsonEncoder
-# publish = False
+dashboard.bind(application)
+
+
+
 
 @application.route('/', methods=['GET'])
 def home():
     return render_template("home.html")
 
 
-@application.route('/htmleditor', methods=['GET'])
-def htmleditor():
-    return render_template("htmleditor.html")
 
 
-@application.route('/latex', methods=['GET'])
-def latex():
+
+@application.route('/html_editor', methods=['GET'])
+def html_editor():
     return render_template("latex.html")
 
 @application.route('/sql', methods=['GET'])
@@ -44,35 +67,13 @@ def sqlsnippet():
     return render_template("sqlSnippet.html", value = '1Iu-zCunodM-l1xH1sU50Huf6BDWoBBBG7xd_7u8plgk', title = 'SQL Live Coding Tutorial Book')
 
 @application.route('/Rdata101', methods=['GET'])
-def rsnippet():
+def Rdata101():
     return render_template("sqlSnippet.html", value = '1g4SFwZuRTO5-4uRuNN_pz3UQaqEoDUMXk0QjTFophJk', title = 'R Live Coding Tutorial Book')
-# @application.route('/datacamp', methods=['GET'])
-# def codeSnippet():
-#     return render_template("datacamp.html")
+
 
 @application.route('/R', methods=['GET'])
 def main_r_page():
     return render_template("main-r.html")
-
-@application.route('/datacamp', methods=['GET'])
-def codeSnippet():
-    sheet_id = '1Iu-zCunodM-l1xH1sU50Huf6BDWoBBBG7xd_7u8plgk'
-    url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet=Sheet1'
-    df=pd.read_csv(url)
-
-    url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet=Sheet2'
-    df1=pd.read_csv(url)
-
-    finaldf=pd.merge(df1, df, on="Section" , how = "outer")
-
-    cols = finaldf.columns.difference(['Section','Name','Details'])
-    d = (finaldf.groupby(['Section','Name','Details'])[cols]
-            .apply(lambda x: x.to_dict(orient='records'))
-            .reset_index(name='Snippets')
-            .to_json(orient='records'))
-
-    return render_template("datacamp.html",value=d)
-
 
 @application.route('/sqlTutorialCode', methods=['POST'])
 def sqlTutorialCode():
@@ -136,6 +137,37 @@ def mongoTutorialCode():
          res.append(x)
     return jsonify(json.dumps({"result":res,"execution":execution_time},cls=JsonEncoder))
 
+
+
+# @application.route('/test', methods=['GET'])
+# def test():
+#     return render_template("sqlSnippet.html", value = '1aX7hU251jONPZsAbcqMOoZBjqDs_0pwuI2OfSPmtApk', title = 'R Live Coding Tutorial Book')
+
+# @application.route('/htmleditor', methods=['GET'])
+# def htmleditor():
+#     return render_template("htmleditor.html")
+
+# @application.route('/datacamp', methods=['GET'])
+# def codeSnippet():
+#     sheet_id = '1aX7hU251jONPZsAbcqMOoZBjqDs_0pwuI2OfSPmtApk'
+#     url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet=Sheet1'
+#     df=pd.read_csv(url)
+
+#     url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet=Sheet2'
+#     df1=pd.read_csv(url)
+
+#     finaldf=pd.merge(df1, df, on="Section" , how = "outer")
+
+#     cols = finaldf.columns.difference(['Section','Name','Details'])
+#     d = (finaldf.groupby(['Section','Name','Details'])[cols]
+#             .apply(lambda x: x.to_dict(orient='records'))
+#             .reset_index(name='Snippets')
+#             .to_json(orient='records'))
+
+#     return render_template("datacamp.html",value=d)
+
+
+
 def getMongoDbConnection():
     client = MongoClient('mongodb+srv://db-mongodb-nyc1-62083-a7614619.mongo.ondigitalocean.com',
                      username='Student',
@@ -162,3 +194,4 @@ def getSqlConnection():
 
 if __name__ == "__main__":
     application.run(port=5001,debug=True)
+#use_reloader=False
